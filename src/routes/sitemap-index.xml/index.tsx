1b7a8b620567e0dafa79f19c19c124ee5983ad58
genekellyboyle <DrDuffy@bhhsnv.com>
2025-09-19 12:32:04 -0700

    Add sitemap index file to manage all sitemaps and update robots.txt
    - Create sitemap-index.xml that references all three sitemaps (main, images, news)
    - Update robots.txt to reference sitemap index instead of individual sitemaps
    - Follow Google's sitemap index best practices for better crawling management
    - Sitemap index includes lastmod dates and proper XML structure

---
+++ src/routes/sitemap-index.xml/index.tsx
@@ -0,0 +1,29 @@
+import { type RequestHandler } from '@builder.io/qwik-city';
+
+export const onGet: RequestHandler = async ({ response }) => {
+  const currentDate = new Date().toISOString().split('T')[0];
+  
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://www.openhouseupdate.com/sitemap.xml</loc>
+    <lastmod>${currentDate}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>https://www.openhouseupdate.com/sitemap-images.xml</loc>
+    <lastmod>${currentDate}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>https://www.openhouseupdate.com/sitemap-news.xml</loc>
+    <lastmod>${currentDate}</lastmod>
+  </sitemap>
+</sitemapindex>`;
+
+  response.headers.set('Content-Type', 'application/xml');
+  response.headers.set('Cache-Control', 'public, max-age=3600');
+  
+  return new Response(sitemapIndex, {
+    status: 200,
+    headers: response.headers,
+  });
+};
