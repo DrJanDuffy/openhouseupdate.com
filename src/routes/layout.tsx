c4fba32eab49a37d2e8ca7d5905530cbe56220b2
genekellyboyle <DrDuffy@bhhsnv.com>
2025-11-02 07:19:02 -0800

    Remove redundant Google Analytics script from layout head

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -328,14 +328,4 @@ export const head: DocumentHead = {
       href: '/sitemap.xml',
     },
   ],
-  scripts: [
-    {
-      children: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-Q9X8KED9X0');
-      `,
-    },
-  ],
 }
